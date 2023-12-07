6229017b985afaa404aa8c121928dd058d32706d
Igor Chornobtyvtsev <igor.c@siklu.com>
2023-12-07 16:21:14 +0200

    print file paths in createDbgSym

--- cmake_adds/DbgSym.cmake
+++ cmake_adds/DbgSym.cmake
@@ -8,11 +8,15 @@ file(MAKE_DIRECTORY ${SYMBOLS_BINARY_PATH})             # Create the given direc
 message("SYMBOLS_BINARY_PATH=${SYMBOLS_BINARY_PATH}")
 function (createDbgSym binFile)
     set (dsFile ${SYMBOLS_BINARY_PATH}/${binFile}.${DEBUG_SYMBOL_SUFFIX})
-    message("binFile    =${binFile}")
-    message("dsFile     =${dsFile}")
 
     add_custom_command(TARGET ${TARGET} POST_BUILD
-            COMMAND ${CMAKE_OBJCOPY} --only-keep-debug $<TARGET_FILE:${binFile}> ${dsFile}
+
+            # COMMENT "igorc this-is-a-comment dsFile=${dsFile}" # only the last comment is printed
+            # COMMAND ${CMAKE_COMMAND} -E echo "igorc dsFile=${dsFile}"
+            COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow "igorc binFile=$<TARGET_FILE:${binFile}>"
+            COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow "igorc dsFile =${dsFile}"
+
+            COMMAND ${CMAKE_OBJCOPY} --only-keep-debug $<TARGET_FILE:${binFile}> ${dsFile} # TARGET_FILE generator expression: Full path to main file (.exe, .so.1.2, .a) where tgt is the name of a target.
             COMMAND ${CMAKE_STRIP} $<TARGET_FILE:${binFile}>
             COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink=${dsFile} $<TARGET_FILE:${binFile}>
     )
